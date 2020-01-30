316a34d84df77f100e61e8f1bf8edb3dd10ce622
Gshelldong <gonglovepj@gmail.com>
2020-01-30 11:11:24 +0800

    关键字删除

--- 自动运维/jumpremote/inspect_words.py
+++ 自动运维/jumpremote/inspect_words.py
@@ -4,15 +4,15 @@ class Kwords():
         self.MaxValue=0.9       # �ڴ� cpu Ӳ�̵����ʹ��ֵ 90%
         self.NormalValue=100     #�Ű���־���ղο�ֵ
         self.set_timeout=4          #��ʱʱ��
-        self.jump_HostIp="61.128.196.123"        #����� IP
-        self.jump_HostPort=1988                 #������˿�
-        self.jump_HostUser='omc'                #������û���
-        self.jump_HostPass='cQIsmsCu1@#'        #���������
-        self.des_Ud_HostUser="ud"               #Ŀ�������û���
-        self.des_Ud_HostPass='cQIsmsCu1@#'
-        self.des_master_user="hadoop"
-        self.des_master_pass='cQIsmsCu1@#'
-        self.curre_username = "hadoop"
+        self.jump_HostIp=""        #����� IP
+        self.jump_HostPort=                 #������˿�
+        self.jump_HostUser=''                #������û���
+        self.jump_HostPass=''        #���������
+        self.des_Ud_HostUser=""               #Ŀ�������û���
+        self.des_Ud_HostPass=''
+        self.des_master_user=""
+        self.des_master_pass=''
+        self.curre_username = ""
         self.ud_lists = ['cu-ud40','cu-ud45','cu-ud46','cu-ud50','cu-ud51','cu-ud52']
         self.master_lists = ["hadoop-master11","hadoop-master12","hadoop-master13",
                              "hadoop-master14","hadoop-master16","hadoop-master17",
